9c044d1b4d971e100b160ee52f6a8e367ddf3193
Aseneca <princeosuh@gmail.com>
2023-02-10 06:09:56 +0100

    basic prototype react blog fully functional

---
+++ src/pages/AboutAuthor.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const AboutAuthor = () => {
+  return (
+    <div>
+      <div className="about-author">
+        <h1>About the Author: Aseneca</h1>
+      </div>
+      <div className="author-image">
+        <img src="./img/author.jpeg" className="hero-image" alt="" />
+      </div>
+      <div>
+        <p>
+          With a background in Physics, understanding the potential of
+          technology has been of great interest to Aseneca. By 2021, Aseneca
+          begun her web development journey by simple HTML and CSS and has grown
+          tremendously to apply advanced programming such as JavaScript and
+          Solidity, as well as Libraries such as React in developing Modern web
+          applications. Aseneca aim to be able to provide sustainable solutions
+          in AI and Blockchain applications and to also guide individuals
+          interested in web development to achieve their goals.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutAuthor;
---
+++ src/pages/AuthorPage.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import AboutAuthor from "./AboutAuthor";
+
+const AuthorPage = () => {
+  return (
+    <div>
+      <Header />
+      <AboutAuthor />
+    </div>
+  );
+};
+
+export default AuthorPage;
